77ea54e51d0e543d842629aafb15c5eb837aee1d
Michal <michalhavlik2@gmail.com>
2025-01-21 20:05:46 +0100

    seeder

--- db/seed.ts
+++ db/seed.ts
@@ -14,13 +14,14 @@ async function main() {
   await prisma.product.createMany({ data: sampleData.products });
 
   // Create sample users and hash their passwords
-  const users = await Promise.all(
-    sampleData.users.map(async (user) => ({
-      ...user,
-      password: await hash(user.password),
-    })),
-  );
-
+  const users = [];
+  for (let i = 0; i < sampleData.users.length; i++) {
+    users.push({
+      ...sampleData.users[i],
+      password: await hash(sampleData.users[i].password),
+    });
+    console.log(sampleData.users[i].password, await hash(sampleData.users[i].password));
+  }
   await prisma.user.createMany({ data: users });
 
   console.log('Database seeded successfully!');
