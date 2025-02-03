8284f8d8ddbb06b445cdec6126f3ccb6b9058277
Michal <michalhavlik2@gmail.com>
2025-02-03 22:20:43 +0100

    Order - Prisma, Zod & Types

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,8 @@ import {
   insertCartSchema,
   cartItemSchema,
   shippingAddressSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
 } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -15,3 +17,14 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string; // it's not in zodSchema bcs we are not adding it in form (it will be error if it was included in chema but not added in form)
+  createdAt: Date;
+  isPaid: Boolean;
+  paidAt: Date | null;
+  isDelivered: Boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
