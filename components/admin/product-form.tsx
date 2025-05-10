167fd49ae5ba5c83876e240fb7ccc6e851d6075b
Michal <michalhavlik2@gmail.com>
2025-05-10 09:55:57 +0200

    Product image upload, IsFeatured, Banner & Update form

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -6,7 +6,7 @@ import { insertProductSchema, updateProductSchema } from '@/lib/validators';
 import { Product } from '@/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
-import { ControllerRenderProps, SubmitErrorHandler, useForm } from 'react-hook-form';
+import { ControllerRenderProps, SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import slugify from 'slugify';
@@ -14,6 +14,10 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { Textarea } from '../ui/textarea';
 import { createProduct, updateProduct } from '@/lib/actions/product.actions';
+import { UploadButton } from '@/lib/uploadthing';
+import { Card, CardContent } from '../ui/card';
+import Image from 'next/image';
+import { Checkbox } from '../ui/checkbox';
 
 const ProductForm = ({
   type,
@@ -30,10 +34,10 @@ const ProductForm = ({
   const form = useForm<z.infer<typeof insertProductSchema>>({
     resolver:
       type === 'Update' ? zodResolver(updateProductSchema) : zodResolver(insertProductSchema),
-    defaultValues: product && type === 'Update' ? product : productDefaultValues, // if updating it will get product valuse, elseif creating it will get default values of product
+    defaultValues: product && type === 'Update' ? product : productDefaultValues,
   });
 
-  const onSubmit: SubmitErrorHandler<z.infer<typeof insertProductSchema>> = async (values) => {
+  const onSubmit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (values) => {
     // On Create
     if (type === 'Create') {
       const res = await createProduct(values);
@@ -47,7 +51,6 @@ const ProductForm = ({
         toast({
           description: res.message,
         });
-
         router.push('/admin/products');
       }
     }
@@ -58,6 +61,7 @@ const ProductForm = ({
         router.push('/admin/products');
         return;
       }
+
       const res = await updateProduct({ ...values, id: productId });
 
       if (!res.success) {
@@ -69,11 +73,14 @@ const ProductForm = ({
         toast({
           description: res.message,
         });
-
         router.push('/admin/products');
       }
     }
   };
+
+  const images = form.watch('images');
+  const isFeatured = form.watch('isFeatured');
+  const banner = form.watch('banner');
 
   return (
     <Form {...form}>
@@ -107,7 +114,7 @@ const ProductForm = ({
               field: ControllerRenderProps<z.infer<typeof insertProductSchema>, 'slug'>;
             }) => (
               <FormItem className='w-full'>
-                <FormLabel>Slug</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
                   <div className='relative'>
                     <Input placeholder='Enter slug' {...field} />
@@ -203,8 +210,93 @@ const ProductForm = ({
             )}
           />
         </div>
-        <div className='upload-field flex flex-col md:flex-row gap-5'>{/* Images */}</div>
-        <div className='upload-field'>{/* isFeatured */}</div>
+        <div className='upload-field flex flex-col md:flex-row gap-5'>
+          {/* Images */}
+          <FormField
+            control={form.control}
+            name='images'
+            render={() => (
+              <FormItem className='w-full'>
+                <FormLabel>Images</FormLabel>
+                <Card>
+                  <CardContent className='space-y-2 mt-2 min-h-48'>
+                    <div className='flex-start space-x-2'>
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt='product image'
+                          className='w-20 h-20 object-cover object-center rounded-sm'
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint='imageUploader'
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue('images', [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast({
+                              variant: 'destructive',
+                              description: `ERROR! ${error.message}`,
+                            });
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className='upload-field'>
+          {/* isFeatured */}
+          Featured Product
+          <Card>
+            <CardContent className='space-y-2 mt-2'>
+              <FormField
+                control={form.control}
+                name='isFeatured'
+                render={({ field }) => (
+                  <FormItem className='space-x-2 items-center'>
+                    <FormControl>
+                      <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                    </FormControl>
+                    <FormLabel>Is Featured?</FormLabel>
+                  </FormItem>
+                )}
+              />
+              {isFeatured && banner && (
+                <Image
+                  src={banner}
+                  alt='banner image'
+                  className='w-full object-cover object-center rounded-sm'
+                  width={1920}
+                  height={680}
+                />
+              )}
+
+              {isFeatured && !banner && (
+                <UploadButton
+                  endpoint='imageUploader'
+                  onClientUploadComplete={(res: { url: string }[]) => {
+                    form.setValue('banner', res[0].url);
+                  }}
+                  onUploadError={(error: Error) => {
+                    toast({
+                      variant: 'destructive',
+                      description: `ERROR! ${error.message}`,
+                    });
+                  }}
+                />
+              )}
+            </CardContent>
+          </Card>
+        </div>
         <div>
           {/* Description */}
           <FormField
@@ -220,8 +312,8 @@ const ProductForm = ({
                 <FormControl>
                   <Textarea
                     placeholder='Enter product description'
-                    {...field}
                     className='resize-none'
+                    {...field}
                   />
                 </FormControl>
                 <FormMessage />
